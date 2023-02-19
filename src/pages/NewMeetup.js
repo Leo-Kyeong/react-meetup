18aa47337e3eb724f1349c1939b811d0377c1b76
Leo <drd9811@naver.com>
2023-02-19 20:42:27 +0900

    Create NewMeetup

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,31 @@
+import {useHistory} from 'react-router-dom';
+
+import NewMeetupForm from "../component/meetups/NewMeetupForm";
+
 function NewMeetupsPage() {
-    return <div>New Meetups Page</div>
+    const history = useHistory();
+
+    function addMeetupHandler(meetupData) {
+        fetch(
+            'https://react-getting-started-practice-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        ).then(() => {
+            history.replace('/');
+        });
+    }
+
+    return (
+        <section>
+            <h1>New Meetups Page</h1>
+            <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+        </section>
+    );
 }
 
 export default NewMeetupsPage;
