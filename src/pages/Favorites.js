75d6788128c68121de6aafa1a4804a416fd43050
Leo <drd9811@naver.com>
2023-02-20 15:38:26 +0900

    Create MyFavorites

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,5 +1,24 @@
+import {useContext} from "react";
+import FavoritesContext from "../store/favorites-context";
+import MeetupList from "../component/meetups/MeetupList";
+
 function FavoritesPage() {
-    return <div>Favorites Page</div>
+    const favoritesCtx = useContext(FavoritesContext);
+
+    let context;
+
+    if (favoritesCtx.totalFavorites === 0) {
+        context = <p>You got no favorites yet. Start adding some?</p>
+    } else {
+        context = <MeetupList meetups={favoritesCtx.favorites} />
+    }
+
+    return (
+        <section>
+            <h1>My Favorites</h1>
+            {context}
+        </section>
+    );
 }
 
 export default FavoritesPage;
